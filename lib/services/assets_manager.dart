8b62a6760ff7d18bd4d5e74b828d6e9a38e287c1
Gopal Verma <vgopaldps@gmail.com>
2023-02-23 14:03:21 +0530

    added assets

---
+++ lib/services/assets_manager.dart
@@ -0,0 +1,6 @@
+class AssetsManager {
+  static String imagePath = "assets";
+  static String userImage = "$imagePath/avatar.png";
+  static String logoImage = "$imagePath/chatgpt-icon.png";
+  static String bgImage = "$imagePath/chatgpt_bg.png";
+}
